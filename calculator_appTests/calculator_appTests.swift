467ccca09f72147b731db3341c277841bfe74184
yamatokawai <ykawai@system-renovate.co.jp>
2025-01-29 14:39:01 +0900

    Initial Commit

---
+++ calculator_appTests/calculator_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appTests.swift
+//  calculator_appTests
+//
+//  Created by 川井やまと on 2025/01/29.
+//
+
+import Testing
+@testable import calculator_app
+
+struct calculator_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
